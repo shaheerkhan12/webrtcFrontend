b5a929d0edc1c15e5b083beb04b958420a315db8
shaheerkhan12 <shaheer.khan@paysky.io>
2023-11-29 03:22:34 +0500

    formsmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WatcherComponent } from './watcher/watcher.component';
 import { BroadcasterComponent } from './broadcaster/broadcaster.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { BroadcasterComponent } from './broadcaster/broadcaster.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
